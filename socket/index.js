f32adba1a19761e4c50b9cbb12a8a5f0615fcc52
Svancik <thesvancik@gmail.com>
2022-05-30 22:19:29 +0200

    socket.io chat

--- socket/index.js
+++ socket/index.js
@@ -3,40 +3,45 @@ const io = require("socket.io")(8900, {
       origin: "http://localhost:3000",
     },
   });
-// iniciace pole objektů users
-let users = [];
-
-// fce na přidání usera do users[] a kontrolu záznamu
-const addUser = (userId, socketId) => {
-    // pomocí .some si namapujeme jednotlivé elementy (= user) pole users a kontrolujeme zda jejich vlastnost .userId neni shodna s arg userId
+  
+  let users = [];
+  
+  const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
-      users.push({userId, socketId});  //pokud shoda není (negace před výrazem výše) = true & může se vykonat přidání userId a socketId do users
-};
-
-// fce na odebrání usera z users[]
-const removeUser = (socketId) =>{
-    users = users.filter(user => user.socketId !== socketId);
-}
-
-
-// napojení na socket.io a odeslání dat klientům
-io.on("connection", (socket)=> {
-    //připojení uživatele
-    console.log("a user connected");
-    io.emit("welcome","hello this is socket server!");
-        //získej userId a socketId od uživatele
-    socket.on("addUser", userId =>{
-        addUser(userId, socket.id);
-        //odešli pole userů s userId a socketId všem klientům
-        io.emit("getUsers", users);
+      users.push({ userId, socketId });
+  };
+  
+  const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+  };
+  
+  const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  
+  io.on("connection", (socket) => {
+    //when ceonnect
+    console.log("a user connected.");
+  
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
     });
-    
-    //odeslání zprávy
-
-
-    //odpojení uživatele
-    socket.on("disconnect", ()=>{
-        console.log("A user was diconnected from Socket.IO");
-        removeUser(socket.id);
-    })
-});
+  
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+      const user = getUser(receiverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    });
+  
+    //when disconnect
+    socket.on("disconnect", () => {
+      console.log("a user disconnected!");
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    });
+  });
